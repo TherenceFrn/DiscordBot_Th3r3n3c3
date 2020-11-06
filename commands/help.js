52966c9db0a3c140c77c1b2163b2cd5b1eb7a638
FERRON Therence <therence.ferron@orange.fr>
2020-11-06 13:58:05 +0100

    feat(help): added help command and feature

---
+++ commands/help.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    description: 'Help',
+    async execute(message) {
+        
+
+        const embed1 = new Discord.MessageEmbed()
+            .setTitle('Liste des commandes :')
+            .setDescription(`##help \n ##play [nounours, pillarmen] \n ##prune [0-99] \n ##server \n ##avatar [empty-@tag]`)
+            .setColor('#FF2D00')
+            .setFooter('(Appuyez sur la croix pour supprimer le message)')
+
+
+        let msg = await message.channel.send(embed1);
+        await msg.react("❌")
+
+        msg.awaitReactions(
+                (reaction, user) => user.id == message.author.id && (reaction.emoji.name == '❌'), {
+                    max: 1,
+                    time: 30000
+                })
+            .then(
+                msg.delete({
+                    timeout: 1000
+                })
+                .then(
+                    message.delete({
+                        timeout: 1000
+                    })
+                )
+            )
+    }
+}
