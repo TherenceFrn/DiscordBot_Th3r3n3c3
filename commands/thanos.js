cc82b168e5459ad1bcc57ab49e6368620cb8f31b
FERRON Therence <therence.ferron@orange.fr>
2020-11-11 01:42:18 +0100

    feat(thanos): thanos audio

--- commands/thanos.js
+++ commands/thanos.js
@@ -5,19 +5,46 @@ module.exports = {
     description: 'Help',
     async execute(message) {
 
-
         const rdm = Math.floor(Math.random() * Math.floor(99));
 
         const embed = new Discord.MessageEmbed()
-            .setTitle('La fatalité de Thanos s\'est abattu sur terre ! 🌍')
+            .setTitle('You should have aimed for the head')
             .setDescription(
                 `${rdm} messages ont été supprimés !`)
-            .setImage('https://media1.giphy.com/media/xUOxeZn47mrdabqDNC/giphy.gif')
+            .setImage('https://media1.tenor.com/images/d89ba4e965cb9144c82348a1c234d425/tenor.gif')
+            .setColor('#FF2D00')
+
+        const embed1 = new Discord.MessageEmbed()
+            .setTitle(`C'est finis !`)
+            .setDescription(`Quelqu'un à décidé de tuer la moitié de la population en demandant l'aide de Thanos`)
+            .setImage('https://media1.tenor.com/images/f039ad91755b2644424967f8f823a91c/tenor.gif?itemid=15240312')
             .setColor('#FF2D00')
 
         let msg = await message.channel.send(embed)
-        await msg.react("👽").then(() =>
-            message.channel.bulkDelet(rdm)
-        )
+        await msg.react("👽")
+
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            const dispatcher = connection.play(`./media/thanos.mp3`, {
+                volume: 1,
+            });
+            dispatcher.on('start', () => {
+                message.client.user.setActivity('tuer la moitié de la population', {
+                    type: 'LISTENING'
+                })
+            })
+
+            dispatcher.on('finish', () => {
+                dispatcher.destroy();
+                message.member.voice.channel.leave();
+                message.client.user.setActivity('baiser zoé', {
+                    type: 'PLAYING'
+                })
+                let msg1 = message.channel.send(embed1)
+
+            })
+        } else {
+            message.reply('Il faut que tu sois dans un channel audio !');
+        }
     }
 }
